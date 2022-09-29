db409beece50e4f74e52093564ee7b87cacc9b71
407 <407@academicos.cesde.edu.co>
2022-09-29 17:17:53 -0500

    se aprende a utilizar react

---
+++ src/helpers/constantesGET.js
@@ -0,0 +1,7 @@
+const TOKEN = " Bearer BQAyO03EUSOvsfm8zeuSHlOLBNnz0OCbIBJsqxGv4W8-5Sh0hVg1qnRElTr9NVVgP6U_oh4PkkjTBY4U0ccTPQ3IKrb9oSN7x36j9rfEIpfbzeCRpl6BoPEl3kSc710ZCEZuGZPAsLItXgGWU_VdY3uh6QpJrVtsJbK8kJwE5FvP8fhNkkX9lqkrEbjVQLvjnqA"
+
+export const PETICION = {
+    method:"GET",
+    headers:{Authorization:TOKEN}
+
+}
